983774770f8b948f395481800f09092968b9ffb2
Parsa Hemmati <parsahemmati20@gmail.com>
2023-08-11 23:14:57 -0400

    Proper package format
    Now available on PyPi!
    Just do pip install parsanlp

--- src/parsanlp/attention.py
+++ src/parsanlp/attention.py
@@ -19,3 +19,21 @@ class AttentionHead(tf.keras.layers.Layer):
         scaled_score = score / tf.math.sqrt(tf.cast(self.dims, float))
         weights = tf.nn.softmax(scaled_score, axis=-1)
         return tf.matmul(weights, vals)
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, h, dmodel):
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.dims = dmodel
+
+        self.heads = [AttentionHead(dmodel) for _ in range(h)]
+
+        self.linear = tf.keras.layers.Dense(self.dims)
+        self.add = tf.keras.layers.Add()
+
+    def call(self, input):
+        res = tf.concat([head(input) for head in self.heads], -1)
+        res = self.linear(res)
+        res = self.add([res, input])
+        return res
