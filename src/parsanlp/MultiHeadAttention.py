32be4c3de75480e17fcface19bbb0c37c92ff40f
Parsa Hemmati <parsahemmati20@gmail.com>
2023-08-11 22:23:55 -0400

    Added Attention And MultiHeadAttention

---
+++ src/parsanlp/MultiHeadAttention.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from Attention import AttentionHead
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+  def __init__(self, h, dmodel):
+    super(MultiHeadAttention, self).__init__()
+    self.h = h
+    self.dims = dmodel
+
+    self.heads = [AttentionHead(dmodel) for _ in range(h)]
+
+    self.linear = tf.keras.layers.Dense(self.dims)
+    self.add = tf.keras.layers.Add()
+
+  def call(self, input):
+    res = tf.concat([head(input) for head in self.heads], -1)
+    res = self.linear(res)
+    res = self.add([res, input])
+    return res
